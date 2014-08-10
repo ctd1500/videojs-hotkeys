7d8850641d30eaac848d28dd588e194d35aed2ee
ctd1500 <ctd1500@gmail.com>
2014-08-10 01:39:41 -0700

    Changing the option names for clarity.

--- videojs.hotkeys.js
+++ videojs.hotkeys.js
@@ -19,16 +19,16 @@
 
     player.on('keydown', function(event) {
       var player = this;
-      var vol = options.vol || 0.1
-      var seek = options.seek || 5
+      var volumeStep = options.volumeStep || 0.1;
+      var seekStep = options.seekStep || 5;
 
       // When controls are disabled, hotkeys will be disabled as well
       if (player.controls()) {
-  
+
         // Don't catch keys if any control buttons are focused
         if (document.activeElement == player.el() ||
             document.activeElement == player.el().querySelector('.vjs-tech')) {
-  
+
           // Spacebar toggles play/pause
           if (event.which === 32) {
             event.preventDefault();
@@ -38,23 +38,23 @@
               player.pause();
             }
           }
-  
+
           // Seeking with the left/right arrow keys
           else if (event.which == 37) { // Left Arrow
             event.preventDefault();
-            player.currentTime(player.currentTime() - seek);
+            player.currentTime(player.currentTime() - seekStep);
           } else if (event.which == 39) { // Right Arrow
             event.preventDefault();
-            player.currentTime(player.currentTime() + seek);
+            player.currentTime(player.currentTime() + seekStep);
           }
-  
+
           // Volume control with the up/down arrow keys
           else if (event.which == 40) { // Down Arrow
             event.preventDefault();
-            player.volume(player.volume() - vol);
+            player.volume(player.volume() - volumeStep);
           } else if (event.which == 38) { // Up Arrow
             event.preventDefault();
-            player.volume(player.volume() + vol);
+            player.volume(player.volume() + volumeStep);
           }
         }
       }
