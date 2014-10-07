e6f1316a929b920f1753172eb784a8d9149f365f
ctd1500 <ctd1500@gmail.com>
2014-10-07 16:43:33 -0700

    Release 0.2.0

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+  var distName = 'videojs-hotkeys';
+  var pkg = grunt.file.readJSON('package.json');
+  var version = pkg.version;
+
+  // Project configuration.
+  grunt.initConfig({
+    baseName: 'videojs.hotkeys',
+    distName: distName,
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      build: ['build/*'],
+      dist: ['dist/*']
+    },
+    uglify: {
+      options: {
+        banner: '/* <%= pkg.name %> v<%= pkg.version %> - <%= pkg.homepage %> */',
+        mangle: true,
+        compress: true,
+        sourceMap: true,
+        sourceMapName: 'build/<%= baseName %>.min.js.map'
+      },
+      build: {
+        src: 'build/<%= baseName %>.js',
+        dest: 'build/<%= baseName %>.min.js'
+      }
+    },
+    copy: {
+      dist: {
+        expand: true,
+        files: [{
+          expand: true,
+          cwd: 'build/',
+          src: '**',
+          dest: 'dist/<%= pkg.name %>/',
+          flatten: true,
+          filter: 'isFile'
+        }]
+      },
+      build: {
+        src: '<%= baseName %>.js',
+        dest: 'build/'
+      }
+    },
+    zip: {
+      dist: {
+        router: function(filepath) {
+          var path = require('path');
+          return path.relative('dist', filepath);
+        },
+        compression: 'DEFLATE',
+        src: ['dist/<%= pkg.name %>/*'],
+        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.zip'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-zip');
+
+  // Default task.
+  grunt.registerTask('default', ['clean', 'copy:build', 'uglify', 'dist', 'cdn-link']);
+
+  grunt.registerTask('dist', 'Creates distribution files', ['copy:dist', 'zip:dist']);
+
+  grunt.registerTask('cdn-link', 'Updates the CDN link in the Readme', function() {
+    var rm = grunt.file.read('README.md');
+    var version = pkg.version;
+
+    version = version.replace(/(\d\.\d)\.\d/, '$1');
+
+    rm = rm.replace(/(\/\/cdn\.sc\.gl\/videojs-hotkeys\/)\d\.\d(\.\d)?/g, '$1' + version);
+    grunt.file.write('README.md', rm);
+  });
+};
--- README.md
+++ README.md
@@ -5,24 +5,41 @@ A plugin for Video.js that enables keyboard hotkeys when the player has focus.
 * Space bar toggles play/pause.
 * Right and Left Arrow keys seek the video forwards and back.
 * Up and Down Arrow keys increase and decrease the volume.
+* M key toggles mute/unmute.
+* F key toggles fullscreen off and on.
 
-###Usage
+## Usage
 Include the plugin:
 
+### CDN version
+You can either load the current release:
 ```
-<script src="videojs.hotkeys.js"></script>
+<script src="//cdn.sc.gl/videojs-hotkeys/0.2/videojs.hotkeys.min.js"></script>
+```
+Or always load the latest version:
+```
+<script src="//cdn.sc.gl/videojs-hotkeys/latest/videojs.hotkeys.min.js"></script>
 ```
 
-Add hotkeys to plugins object.
+### Self hosted
+```
+<script src="/path/to/videojs.hotkeys.js"></script>
+```
 
-    plugins: {
-      hotkeys: {
+Add hotkeys to your Videojs ready function.
+
+    videojs('vidId').ready(function() {
+      this.hotkeys({
         volumeStep: 0.1,
-        seekStep: 5
-      }
-    }
+        seekStep: 5,
+        enableMute: true,
+        enableFullscreen: true
+      });
+    });
 
 ## Options
 
 - `volumeStep` (decimal): The percentage to increase/decrease the volume level when using the Up and Down Arrow keys (default: `0.1`)
 - `seekStep` (integer): The number of seconds to seek forward and backwards when using the Right and Left Arrow keys (default: `5`)
+- `enableMute` (boolean): Enables the volume mute to be toggle by pressing the **M** key (default: `true`)
+- `enableFullscreen` (boolean): Enables toggling the video fullscreen by pressing the **F** key (default: `true`)
