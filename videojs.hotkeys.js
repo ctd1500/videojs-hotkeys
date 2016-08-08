25ee21ef601b4aa5bf3eecfd7aeb65e7d834e607
ctd1500 <ctd1500@gmail.com>
2016-08-08 16:57:11 -0700

    Release 0.2.15

--- videojs.hotkeys.js
+++ videojs.hotkeys.js
@@ -17,7 +17,7 @@
 
 })(window, function(window, videojs) {
   "use strict";
-  window['videojs_hotkeys'] = { version: "0.2.14" };
+  window['videojs_hotkeys'] = { version: "0.2.15" };
 
   var hotkeys = function(options) {
     var player = this;
