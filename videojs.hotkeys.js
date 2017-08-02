ed2a641ac2f0529a2cfed13bcade36403f0893ad
ctd1500 <ctd1500@gmail.com>
2017-08-02 15:11:42 -0700

    Release 0.2.20

--- videojs.hotkeys.js
+++ videojs.hotkeys.js
@@ -8,18 +8,18 @@
 
 ;(function(root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define('videojs-hotkeys', ["video.js"], function (module) {
+    define('videojs-hotkeys', ['video.js'], function (module) {
       return factory(module.default || module);
     });
   } else if (typeof module !== 'undefined' && module.exports) {
-    module.exports = factory(require("video.js"));
+    module.exports = factory(require('video.js'));
   } else {
     factory(videojs);
   }
 }(this, function (videojs) {
   "use strict";
   if (typeof window !== 'undefined') {
-    window['videojs_hotkeys'] = { version: "0.2.19" };
+    window['videojs_hotkeys'] = { version: "0.2.20" };
   }
 
   var hotkeys = function(options) {
